70d943767cfa6ebb3b9782bb4798ba6f09a5fa86
ahmedallani <ahmed.allani@rbk.tn>
2020-09-24 15:29:27 +0100

    modal

--- js/destination.js
+++ js/destination.js
@@ -1,11 +1,30 @@
+function maxWidth(){
+  return Math.max(
+    document.documentElement["clientWidth"],
+    document.body["scrollWidth"],
+    document.documentElement["scrollWidth"],
+    document.body["offsetWidth"],
+    document.documentElement["offsetWidth"]
+  );
+}
 var main = document.getElementById("main");
+var sidenav = document.getElementById("sidenav");
+var openSideNav = document.getElementById("openSideNav");
 main.onclick = function(event) {
-  console.log("window.onclick");
-  var sidenav = document.getElementById("sidenav");
-  var openSideNav = document.getElementById("openSideNav");
   if (openSideNav.checked) {
     if (!sidenav.contains(event.target)) {
       openSideNav.checked = false;
     }
   }
 };
+console.log("gggg")
+var body=document.body
+window.onload = function(){
+  var max=maxWidth()
+  console.log({max})
+  if(max>=768){
+    openSideNav.checked = true;
+  } else {
+    openSideNav.checked = false;
+  }
+}
